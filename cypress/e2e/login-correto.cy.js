ad3589fe7ef97260ea87682a41d11f15675db0e5
marielless <mariellesantos96@hotmail.com>
2025-01-16 14:32:50 -0800

    Login correto e documentação mochawesome

--- cypress/e2e/login-correto.cy.js
+++ cypress/e2e/login-correto.cy.js
@@ -1,7 +1,10 @@
 describe('Página de login', () => {
-    it('Deve preencher os campos do login corretamente e autenticar o usuário na página', () => {
+    beforeEach(() => {
         cy.visit('https://adopet-frontend-cypress.vercel.app/');
         cy.get('[data-test="login-button"]').click();
+    })
+
+    it('Deve preencher os campos do login corretamente e autenticar o usuário na página', () => {
         cy.get('[data-test="input-loginEmail"]').type('mari.santos@teste.com');
         cy.get('[data-test="input-loginPassword"]').type('senhaSegura123');
         cy.get('[data-test="submit-button"]').click();
